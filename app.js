593949b4c39f2f424e6cbaff6e7c4d7837429527
Anirudhk94 <k.anirudh8@gmail.com>
2017-05-05 14:16:44 +0530

    Added logic for #stores

--- app.js
+++ app.js
@@ -8,151 +8,212 @@ app.controller('myCtrl', function($scope, $http) {
         }
     ];
 
-    $scope.tree_data = [{  
-        "Name":"USA",
-        "Product id":9826675,
-        "Mean1":123,
-        "SD1":318,
-        "Mean2":234,
-        "SD2":123,
-        "stores":6,
-        "Berg":"5/10",
-        "Karl":"6/10",
-        "HH34":"3/10",
-        "Average":41,
-        "Average2":39,
-        "children":[  
-            {  
-                "Name":"Store1",
-                "Product id":12332,
-                "Mean1":323,
-                "SD1":3545,
-                "Mean2":32,
-                "SD2":1,
-                "stores":3,
-                "Berg":"1/3",
-                "Karl":"2/3",
-                "HH34":1,
-                "Average":2,
-                "Average2":1
-            },
-            {  
-                "Name":"Store2",
-                "Product id":24341,
-                "Mean1":442,
-                "SD1":543,
-                "Mean2":2412,
-                "SD2":2424,
-                "stores":2,
-                "Berg":"1/2",
-                "Karl":1,
-                "HH34":"1/2",
-                "Average":1.33,
-                "Average2":1.5
-            },
-            {  
-                "Name":"Hello",
-                "Product id":84272,
-                "Mean1":234,
-                "SD1":455,
-                "Mean2":643,
-                "SD2":4265,
-                "stores":5,
-                "Berg":1,
-                "Karl":"4/5",
-                "HH34":1,
-                "Average":4.66,
-                "Average2":4.5,
-                "children":[  
-                    {  
-                    "Name":"Eclipse",
-                    "Product id":89241,
-                    "Mean1":124,
-                    "SD1":24553,
-                    "Mean2":5124,
-                    "SD2":214,
-                    "stores":3,
-                    "Berg":1,
-                    "Karl":"",
-                    "HH34":1,
-                    "Average":0.6,
-                    "Average2":0.5
-                    },
-                    {  
-                    "Name":"Circle",
-                    "Product id":122321,
-                    "Mean1":1256,
-                    "SD1":643,
-                    "Mean2":667,
-                    "SD2":86,
-                    "stores":5,
-                    "Berg":1,
-                    "Karl":1,
-                    "HH34":1,
-                    "Average":1,
-                    "Average2":1
-                    },
-                    {  
-                    "Name":"Square",
-                    "Product id":8653,
-                    "Mean1":65,
-                    "SD1":86,
-                    "Mean2":643,
-                    "SD2":733,
-                    "stores":5,
-                    "Berg":1,
-                    "Karl":1,
-                    "HH34":1,
-                    "Average":1,
-                    "Average2":1
-                    },
-                    {  
-                    "Name":"Rhombus",
-                    "Product id":75368,
-                    "Mean1":456,
-                    "SD1":46,
-                    "Mean2":6245,
-                    "SD2":72,
-                    "stores":5,
-                    "Berg":1,
-                    "Karl":1,
-                    "HH34":1,
-                    "Average":1,
-                    "Average2":1
-                    },
-                    {  
-                    "Name":"Parallel",
-                    "Product id":094217,
-                    "Mean1":7644,
-                    "SD1":62,
-                    "Mean2":51,
-                    "SD2":53,
-                    "stores":5,
-                    "Berg":1,
-                    "Karl":1,
-                    "HH34":1,
-                    "Average":1,
-                    "Average2":1
-                    }
-                ]
-            }
-        ]
-        },
-        {  
-        "Name":"Parallello",
-        "Product id":0942121,
-        "Mean1":721,
-        "SD1":62,
-        "Mean2":51,
-        "SD2":53,
-        "stores":5,
-        "Berg":"4/10",
-        "Karl":"3/10",
-        "HH34":"5/10",
-        "Average":3,
-        "Average2":3.5
-        }
-        ];
+    $scope.tree_data = [  
+   {  
+      "Name":"USA",
+      "Product id":"",
+      "Mean1":123,
+      "SD1":318,
+      "Mean2":234,
+      "SD2":123,
+      "stores":6,
+      "Berg":"5/10",
+      "Karl":"6/10",
+      "HH34":"3/10",
+      "Average":41,
+      "Average2":39,
+      "children":[  
+         {  
+            "Name":"store1",
+            "Product id":"",
+            "Mean1":323,
+            "SD1":3545,
+            "Mean2":32,
+            "SD2":1,
+            "stores":5,
+            "Berg":"1/3",
+            "Karl":"2/3",
+            "HH34":1,
+            "Average":2,
+            "Average2":1,
+            "children":[  
+               {  
+                  "Name":"store1.1",
+                  "Product id":985327,
+                  "Mean1":312,
+                  "SD1":35,
+                  "Mean2":635,
+                  "SD2":12,
+                  "stores":5,
+                  "Berg":1,
+                  "Karl":1,
+                  "HH34":1,
+                  "Average":1,
+                  "Average2":1
+               },
+               {  
+                  "Name":"store1.2",
+                  "Product id":65425,
+                  "Mean1":543,
+                  "SD1":21,
+                  "Mean2":123,
+                  "SD2":45,
+                  "stores":3,
+                  "Berg":"",
+                  "Karl":1,
+                  "HH34":1,
+                  "Average":0.6,
+                  "Average2":0.5
+               },
+               {  
+                  "Name":"store1.3",
+                  "Product id":453268,
+                  "Mean1":325,
+                  "SD1":21,
+                  "Mean2":986,
+                  "SD2":25,
+                  "stores":1,
+                  "Berg":"",
+                  "Karl":"",
+                  "HH34":1,
+                  "Average":0.3,
+                  "Average2":""
+               }
+            ]
+         },
+         {  
+            "Name":"store2",
+            "Product id":"",
+            "Mean1":442,
+            "SD1":543,
+            "Mean2":2412,
+            "SD2":2424,
+            "stores":2,
+            "Berg":"1/2",
+            "Karl":1,
+            "HH34":"1/2",
+            "Average":1.33,
+            "Average2":1.5,
+            "children":[  
+               {  
+                  "Name":"store2.1",
+                  "Product id":96542,
+                  "Mean1":1001,
+                  "SD1":23,
+                  "Mean2":6035,
+                  "SD2":62,
+                  "stores":3,
+                  "Berg":"",
+                  "Karl":1,
+                  "HH34":1,
+                  "Average":0.6,
+                  "Average2":0.5
+               },
+               {  
+                  "Name":"store2.2",
+                  "Product id":703541,
+                  "Mean1":5020,
+                  "SD1":13,
+                  "Mean2":3650,
+                  "SD2":24,
+                  "stores":4,
+                  "Berg":1,
+                  "Karl":1,
+                  "HH34":"",
+                  "Average":0.6,
+                  "Average2":1
+               }
+            ]
+         },
+         {  
+            "Name":"hello",
+            "Product id":"",
+            "Mean1":234,
+            "SD1":455,
+            "Mean2":643,
+            "SD2":4265,
+            "stores":5,
+            "Berg":1,
+            "Karl":"4/5",
+            "HH34":1,
+            "Average":4.66,
+            "Average2":4.5,
+            "children":[  
+               {  
+                  "Name":"eclipse",
+                  "Product id":89241,
+                  "Mean1":124,
+                  "SD1":24553,
+                  "Mean2":5124,
+                  "SD2":214,
+                  "stores":3,
+                  "Berg":1,
+                  "Karl":"",
+                  "HH34":1,
+                  "Average":0.6,
+                  "Average2":0.5
+               },
+               {  
+                  "Name":"circle",
+                  "Product id":122321,
+                  "Mean1":1256,
+                  "SD1":643,
+                  "Mean2":667,
+                  "SD2":86,
+                  "stores":5,
+                  "Berg":1,
+                  "Karl":1,
+                  "HH34":1,
+                  "Average":1,
+                  "Average2":1
+               },
+               {  
+                  "Name":"square",
+                  "Product id":8653,
+                  "Mean1":65,
+                  "SD1":86,
+                  "Mean2":643,
+                  "SD2":733,
+                  "stores":5,
+                  "Berg":1,
+                  "Karl":1,
+                  "HH34":1,
+                  "Average":1,
+                  "Average2":1
+               },
+               {  
+                  "Name":"rhombus",
+                  "Product id":75368,
+                  "Mean1":456,
+                  "SD1":46,
+                  "Mean2":6245,
+                  "SD2":72,
+                  "stores":5,
+                  "Berg":1,
+                  "Karl":1,
+                  "HH34":1,
+                  "Average":1,
+                  "Average2":1
+               },
+               {  
+                  "Name":"parallel",
+                  "Product id":094217,
+                  "Mean1":7644,
+                  "SD1":62,
+                  "Mean2":51,
+                  "SD2":53,
+                  "stores":5,
+                  "Berg":1,
+                  "Karl":1,
+                  "HH34":1,
+                  "Average":1,
+                  "Average2":1
+               }
+            ]
+         }
+      ]
+   }
+];
 
     $scope.selectedRow = null;
 
@@ -231,51 +292,42 @@ app.controller('myCtrl', function($scope, $http) {
     var initializeData = function() {
         console.log('In initializeData()')
         for (i = 0; i < $scope.tree_data.length; i++) { //USA & India
-            $scope.tree_data[i].selected = false
+            $scope.tree_data[i].expanded = true
+            var temp = 0
             if ($scope.tree_data[i].children != null && $scope.tree_data[i].children.length > 0) { //If level 1 has children
                 var count = 0; //Count variable to check level 3 nodes that are 0
                 console.log($scope.tree_data[i].Name + " has children")
                 for (j = 0; j < $scope.tree_data[i].children.length; j++) { //Cali & Illinois
-                    $scope.tree_data[i].children[j].selected = false
+                    $scope.tree_data[i].children[j].expanded = true
                     if ($scope.tree_data[i].children[j].children != null && Object.keys($scope.tree_data[i].children[j].children).length > 0) { //If level 2 has children
                         
                         console.log($scope.tree_data[i].children[j].Name + " has children")
-                        var completed = 0
-
+                        count = 0
                         for (k = 0; k < Object.keys($scope.tree_data[i].children[j].children).length; k++) {
-                            $scope.tree_data[i].children[j].children[k].selected = false;
-                            if ($scope.tree_data[i].children[j].children[k].A == 1) {
-                                completed++
+                            $scope.tree_data[i].children[j].children[k].expanded = true;
+                            completed = 0
+                            if ($scope.tree_data[i].children[j].children[k].Berg == 1) {
+                                completed += 2
                             }
+                            if ($scope.tree_data[i].children[j].children[k].Karl == 1) {
+                                completed += 2
+                            }
+                            if ($scope.tree_data[i].children[j].children[k].HH34 == 1) {
+                                completed += 1
+                            }
+                            count += completed
+                            $scope.tree_data[i].children[j].children[k].stores = completed
                         }
 
-                        if (Object.keys($scope.tree_data[i].children[j].children).length == completed) {
-                            $scope.tree_data[i].children[j].A = 1
-                        } else {
-                            $scope.tree_data[i].children[j].A = completed + "/" + Object.keys($scope.tree_data[i].children[j].children).length
-                        }
-                        console.log($scope.tree_data[i].children[j].Name + " : " + $scope.tree_data[i].children[j].A)
-
-                        console.log($scope.tree_data[i].Name + " > " + $scope.tree_data[i].children[j].Name + ".A : " + $scope.tree_data[i].children[j].A)
-
-                        if ($scope.tree_data[i].children[j].A == 1) {
-                            count++
-                        }
+                        $scope.tree_data[i].children[j].stores = Math.floor(count / Object.keys($scope.tree_data[i].children[j].children).length)
+                        console.log(count)
+                        temp += $scope.tree_data[i].children[j].stores
                     }
                 }
-
-                console.log($scope.tree_data[i].Name + "'s count = " + count)
-
-                if ($scope.tree_data[i].length == count) {
-                    $scope.tree_data[i].A = count
-                } else {
-                    $scope.tree_data[i].A = count + "/" + $scope.tree_data[i].children.length
-                }
-                console.log($scope.tree_data[i].Name + " : " + $scope.tree_data[i].A)
+                console.log("temp : "+ temp)
+                $scope.tree_data[i].stores = Math.floor(temp / $scope.tree_data[i].children.length)
             }
-            else {
-                $scope.tree_data[i].A = 1
-            }
+
         }
     }
 
